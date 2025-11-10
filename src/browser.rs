15536dc87649c92b5f55a69f07bff209263f47c6
Wreck-X <hemanthkrishnakumar@gmail.com>
2025-11-10 18:52:33 +0530

    refactor: modularize main application into App struct
    Extract main GTK setup logic into new `App` struct under `src/app.rs`.
    Move browser and UI initialization into `App::run` which simplifies
    `main.rs` to only create and run the application.

--- src/browser.rs
+++ src/browser.rs
@@ -1,34 +1,34 @@
-use gtk::prelude::*;
-use gtk::gio;
 use gtk::Box as GtkBox;
+use gtk::gio;
 use gtk::Orientation;
-use webkit2gtk::{WebView,WebViewExt, WebContext, LoadEvent};
+use gtk::prelude::*;
+use webkit2gtk::{LoadEvent, WebContext, WebView, WebViewExt};
 
 pub struct Browser {
     pub webview: WebView,
-} 
+}
 
 impl Browser {
     pub fn new() -> Self {
-        let context  = WebContext::default().expect("Failed to get WebContext");
+        let context = WebContext::default().expect("Failed to get WebContext");
         let webview = WebView::with_context(&context);
 
-        Browser {webview}
+        Browser { webview }
     }
 
     pub fn load_url(&self, url: &str) {
         self.webview.load_uri(url);
-    } 
+    }
 
     pub fn inject_link_parser(&self) {
         let js_code = crate::js::link_parser::LINK_PARSER_JS;
-        self.webview.connect_load_changed(move |webview, load_event| {
-            if load_event == LoadEvent::Finished {
-                webview.run_javascript(js_code, None::<&gio::Cancellable>, |_| ());
-            }
-        });
+        self.webview
+            .connect_load_changed(move |webview, load_event| {
+                if load_event == LoadEvent::Finished {
+                    webview.run_javascript(js_code, None::<&gio::Cancellable>, |_| ());
+                }
+            });
     }
-
 
     pub fn build_ui(&self) -> GtkBox {
         let vbox = GtkBox::new(Orientation::Vertical, 0);
@@ -36,4 +36,3 @@ impl Browser {
         vbox
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
-use gtk::prelude::*;
-use gtk::{ApplicationWindow, Application, Box as GtkBox, Orientation};
+mod app;
+mod browser;
+mod js;
+mod ui;
 
-use webkit2gtk::{WebView,WebViewExt,WebContext};
-
-fn main () {
-    let app = Application::builder()
-        .application_id("application.browser")
-        .build();
-    
-    app.connect_activate(|app|{
-        let vbox = GtkBox::new(Orientation::Vertical, 0); 
-        let context = WebContext::default().expect("could not get default WebContext");
-        let webview = WebView::with_context(&context);
-
-        webview.load_uri("https://google.com");
-        vbox.pack_start(&webview, true, true, 0);
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .title("le browser")
-            .default_width(400)
-            .default_height(300)
-            .child(&vbox)
-            .build();
-        
-        win.show_all();
-    });
-    
+use app::App;
+fn main() {
+    let app = App::new();
     app.run();
 }
